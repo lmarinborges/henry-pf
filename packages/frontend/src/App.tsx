ac916ee967b4e62e3d3448a4dacd395004a6a841
Liam Marin <liam.marin03926@protonmail.com>
2023-03-30 01:51:10 -0300

    feat(frontend): set up page layouts

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -6,10 +6,12 @@ import {
   Route,
   RouterProvider,
 } from "react-router-dom";
+import AdminLayout from "./components/AdminLayout";
+import PublicLayout from "./components/PublicLayout";
 
 const routes = createBrowserRouter(
   createRoutesFromElements([
-    <Route path="/" element={<h1>Public Layout</h1>}>
+    <Route path="/" element={<PublicLayout />}>
       <Route index element={<Navigate to="/home" />} />
       <Route path="home" element={<h1>Landing Page</h1>} />
       <Route path="about" element={<h1>Sobre Nosotros</h1>} />
@@ -18,7 +20,7 @@ const routes = createBrowserRouter(
         <Route path=":productId" element={<h1>Información de Producto</h1>} />
       </Route>
     </Route>,
-    <Route path="/admin" element={<h1>Layout de Administrador</h1>}>
+    <Route path="/admin" element={<AdminLayout />}>
       <Route path="products" element={<h1>Administración de Productos</h1>} />
     </Route>,
   ])
