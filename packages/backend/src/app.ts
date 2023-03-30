632feaa3fbc2338599380e71fb145fc1bfc150e2
Liam Marin <liam.marin03926@protonmail.com>
2023-03-30 01:12:13 -0300

    feat(backend): apply middleware and routes

--- packages/backend/src/app.ts
+++ packages/backend/src/app.ts
@@ -1,8 +1,14 @@
 import express from "express";
+import morgan from "morgan";
 import productsRouter from "./modules/products";
 
 const app = express();
 
+// Middleware
+app.use(morgan("dev"));
+app.use(express.json());
+
+// Routes
 app.use(productsRouter);
 
 export default app;
