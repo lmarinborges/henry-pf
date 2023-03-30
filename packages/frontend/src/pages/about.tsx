a2cf1c800e4ea53a15c7d246ae7c2dbf1f20e1de
unknown <tomyguagnini@gmail.com>
2023-03-30 14:19:42 -0300

    build: about and not found page

--- packages/frontend/src/pages/about.tsx
+++ packages/frontend/src/pages/about.tsx
@@ -1,3 +1,35 @@
+import { Text, Flex, Box, Image } from "@chakra-ui/react";
+import cohete from "../assets/cohete.png";
+import crossfit from "../assets/crossfit.png";
+import pay from "../assets/pagar.png";
 export default function AboutPage() {
-  return <h1>Sobre Nosotros</h1>;
+    return (
+        <Flex direction="column" alignItems="center">
+            <Text fontSize="4xl" textAlign='center'>¿Porque elegirnos?</Text>
+            <Image
+                boxSize="100px"
+                objectFit="cover"
+                src={crossfit}
+                alt="cohete"
+                mb='5'
+            />
+            <Text maxW={300} textAlign='center' mb='10'>La mayor variedad de articulos deportivo del pais</Text>
+            <Image
+                boxSize="100px"
+                objectFit="cover"
+                src={pay}
+                alt="cohete"
+                mb='5'
+            />
+            <Text maxW={300} textAlign='center' mb='10'>Paga de manera facil y sencillo con mercado pago</Text>
+            <Image
+                boxSize="100px"
+                objectFit="cover"
+                src={cohete}
+                alt="cohete"
+                mb='5'
+            />
+            <Text maxW={300} textAlign='center' mb='10'>Enviamos los productos a TODO EL PAIS</Text>
+        </Flex>
+    );
 }
