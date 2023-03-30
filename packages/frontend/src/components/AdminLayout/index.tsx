89ed1e98f7dbb9f59eb3349ba29dd1aade94aae3
Liam Marin <liam.marin03926@protonmail.com>
2023-03-30 15:11:51 -0300

    feat(frontend): add Navbar to admin layout

--- packages/frontend/src/components/AdminLayout/index.tsx
+++ packages/frontend/src/components/AdminLayout/index.tsx
@@ -1,11 +1,21 @@
+import { Container } from "@chakra-ui/react";
 import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
+
+const NAVBAR_HEIGHT = "48px";
 
 export default function AdminLayout() {
   return (
     <>
-      <Navbar />
-      <Outlet />
+      <Navbar height={NAVBAR_HEIGHT} />
+      <Container
+        size="sm"
+        px="3"
+        pb="3"
+        pt={`calc(${NAVBAR_HEIGHT} + var(--chakra-sizes-3))`}
+      >
+        <Outlet />
+      </Container>
     </>
   );
 }
