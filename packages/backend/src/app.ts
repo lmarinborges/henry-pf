6d7340091b68beb5b70a36590a6c1648bdf381dc
MariaIsabelGonzalez <gm2021g@gmail.com>
2023-03-30 14:59:49 -0300

    feat(backend-brand-router-contr): create router and controller of brand

--- packages/backend/src/app.ts
+++ packages/backend/src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import "express-async-errors";
 import morgan from "morgan";
 import productsRouter from "./modules/products";
+import brandsRouter from "./modules/brands";
 
 const app = express();
 
@@ -11,5 +12,6 @@ app.use(express.json());
 
 // Routes
 app.use(productsRouter);
+app.use(brandsRouter);
 
 export default app;
