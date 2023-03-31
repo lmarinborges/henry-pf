733cac4a08406ae75e79f727fd785e4719ccd9f9
Liam Marin <liam.marin03926@protonmail.com>
2023-03-31 18:38:10 -0300

    feat(backend): add schema parsing to brands

--- packages/backend/src/modules/brands/brands.controller.ts
+++ packages/backend/src/modules/brands/brands.controller.ts
@@ -1,50 +1,53 @@
 import { Request, Response } from "express";
+import { z } from "zod";
 import prisma from "../../db";
 
-// Devuelve todos las marcas.
+const paramsSchema = z.object({
+  brandId: z.coerce.number(),
+});
+
+const createSchema = z.object({
+  name: z.string().min(1),
+  description: z.string().min(1).optional(),
+});
+
+const updateSchema = z.object({
+  params: paramsSchema,
+  body: createSchema.partial(),
+});
+
 export async function getAllBrands(req: Request, res: Response) {
   const brands = await prisma.brand.findMany();
   res.status(200).json(brands);
 }
 
-// Devuelve un marca basado en su ID.
 export async function getBrand(req: Request, res: Response) {
-  const { brandId } = req.params;
+  const { brandId } = await paramsSchema.parseAsync(req.params);
   const brand = await prisma.brand.findUnique({
-    where: { id: parseInt(brandId) },
+    where: { id: brandId },
   });
   res.status(200).json(brand);
 }
 
-// Actualiza un marca basado en su ID.
 export async function updateBrand(req: Request, res: Response) {
-  const { brandId } = req.params;
-  const data: {
-    name: string;
-    description: string;
-  } = req.body;
+  const { body, params } = await updateSchema.parseAsync(req);
   const brand = await prisma.brand.update({
-    where: { id: parseInt(brandId) },
-    data,
+    where: { id: params.brandId },
+    data: body,
   });
   res.status(200).json(brand);
 }
 
-// Borrado físico, validar si existe marca con esa marca.
 export async function deleteBrand(req: Request, res: Response) {
-  const { brandId } = req.params;
+  const { brandId } = await paramsSchema.parseAsync(req.params);
   const brand = await prisma.brand.delete({
-    where: { id: parseInt(brandId) },
+    where: { id: brandId },
   });
   res.status(200).json(brand);
 }
 
-// Crear marca
 export async function createBrand(req: Request, res: Response) {
-  const data: {
-    name: string;
-    description: string;
-  } = req.body;
-  const oneBrand = await prisma.brand.create({ data });
-  res.status(200).json(oneBrand);
+  const data = await createSchema.parseAsync(req.body);
+  const brand = await prisma.brand.create({ data });
+  res.status(200).json(brand);
 }
