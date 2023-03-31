17138622f170bd06ba10a223f236bb79047718a2
Liam Marin <liam.marin03926@protonmail.com>
2023-03-31 18:04:23 -0300

    feat(backend): add 404 and 500 status handlers

--- packages/backend/src/app.ts
+++ packages/backend/src/app.ts
@@ -1,9 +1,10 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import "express-async-errors";
 import morgan from "./middleware/morgan";
 import brandsRouter from "./modules/brands";
 import categoriesRouter from "./modules/categories";
 import productsRouter from "./modules/products";
+import logger from "./utils/logger";
 
 const app = express();
 
@@ -15,5 +16,28 @@ app.use(express.json());
 app.use(productsRouter);
 app.use(brandsRouter);
 app.use(categoriesRouter);
+
+// 404 Handler
+app.use((req, res) => {
+  res.send(404).json({
+    status: 404,
+    message: "Requested resource does not exist.",
+  });
+});
+
+// Error handler
+app.use((err: unknown, req: Request, res: Response, next: NextFunction) => {
+  if (res.headersSent) return next(err);
+  if (err instanceof Error) {
+    logger.error(err.message);
+    logger.debug(err.stack);
+  } else {
+    logger.error(err);
+  }
+  res.status(500).send({
+    status: 500,
+    message: "Internal server error.",
+  });
+});
 
 export default app;
