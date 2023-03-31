1c1522fdadd8e53c8c4fc1e4db70582a666df770
Liam Marin <liam.marin03926@protonmail.com>
2023-03-31 18:14:42 -0300

    fix(backend): fix extra newline on morgan logs

--- packages/backend/src/middleware/morgan.ts
+++ packages/backend/src/middleware/morgan.ts
@@ -3,7 +3,10 @@ import logger from "../utils/logger";
 
 const morgan = () =>
   morganLib("dev", {
-    stream: { write: (message) => logger.http(message) },
+    stream: {
+      write: (message) =>
+        logger.http(message.substring(0, message.lastIndexOf("\n"))),
+    },
     skip: () => (process.env.NODE_ENV || "development") !== "development",
   });
 
