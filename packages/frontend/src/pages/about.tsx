1b093c5cf84d0a099c6f5858f99db2f8e04b48e0
unknown <tomyguagnini@gmail.com>
2023-03-31 14:26:57 -0300

    style(front/about): font size

--- packages/frontend/src/pages/about.tsx
+++ packages/frontend/src/pages/about.tsx
@@ -1,35 +1,35 @@
-import { Text, Flex, Box, Image } from "@chakra-ui/react";
+import { Text, Flex, Image } from "@chakra-ui/react";
 import cohete from "../assets/cohete.png";
 import crossfit from "../assets/crossfit.png";
 import pay from "../assets/pagar.png";
 export default function AboutPage() {
     return (
         <Flex direction="column" alignItems="center">
-            <Text fontSize="4xl" textAlign='center'>¿Porque elegirnos?</Text>
+            <Text fontSize="6xl" fontWeight={600} textAlign='center'>¿Porque elegirnos?</Text>
             <Image
-                boxSize="100px"
+                boxSize="150px"
                 objectFit="cover"
                 src={crossfit}
                 alt="cohete"
                 mb='5'
             />
-            <Text maxW={300} textAlign='center' mb='10'>La mayor variedad de articulos deportivo del pais</Text>
+            <Text fontSize='2xl' fontWeight={400} textAlign='center' mb='10'>La mayor variedad de articulos deportivo del pais</Text>
             <Image
-                boxSize="100px"
+                boxSize="150px"
                 objectFit="cover"
                 src={pay}
                 alt="cohete"
                 mb='5'
             />
-            <Text maxW={300} textAlign='center' mb='10'>Paga de manera facil y sencillo con mercado pago</Text>
+            <Text fontSize='2xl' textAlign='center' mb='10'>Paga de manera facil y sencillo con mercado pago</Text>
             <Image
-                boxSize="100px"
+                boxSize="150px"
                 objectFit="cover"
                 src={cohete}
                 alt="cohete"
                 mb='5'
             />
-            <Text maxW={300} textAlign='center' mb='10'>Enviamos los productos a TODO EL PAIS</Text>
+            <Text fontSize='2xl' textAlign='center' mb='10'>Enviamos los productos a TODO EL PAIS</Text>
         </Flex>
     );
 }
