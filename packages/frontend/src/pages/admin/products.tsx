2eef90931369aa57729c985c4caa81dce9852bbd
rojasxd2308 <raulrojascadilloxd@gmail>
2023-04-02 16:29:18 -0500

    feat: connect product cards to backend

--- packages/frontend/src/pages/admin/products.tsx
+++ packages/frontend/src/pages/admin/products.tsx
@@ -1,3 +1,35 @@
+import { Flex } from "@chakra-ui/react";
+import Card from "../products/card";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 export default function ProductsAdminPage() {
-  return <h1>Administración de Productos</h1>;
+  const [Allproducts, setAllProducts] = useState({
+    totalItems: "",
+    pageSize: "",
+    products: [],
+  });
+
+  useEffect(() => {
+    const fetchproducts = async () => {
+      try {
+        const response = await axios.get("http://localhost:4000/products");
+        setAllProducts(response.data);
+        console.log(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchproducts();
+  }, []);
+
+  return (
+    <Flex alignItems="center" justifyContent="center" dir="row" wrap="wrap">
+      {Allproducts.products.map((e) => {
+        e = { ...e, price: parseFloat(e.price), rating: 4.5, numReviews: 34 };
+        return <Card product={e} key={e.id}></Card>;
+      })}
+    </Flex>
+  );
 }
--- packages/frontend/src/pages/products/index.tsx
+++ packages/frontend/src/pages/products/index.tsx
@@ -2,50 +2,50 @@ import { Flex } from "@chakra-ui/react";
 import Card from "./card";
 
 const data = [
-    {
-        isNew: true,
-        imageURL:
-            "https://http2.mlstatic.com/D_NQ_NP_2X_746024-MLA48945892843_012022-F.webp",
-        name: "Whey Protein",
-        price: 4.5,
-        rating: 4.5,
-        numReviews: 34,
-    },
-    {
-        isNew: true,
-        imageURL:
-            "https://http2.mlstatic.com/D_NQ_NP_2X_709580-MLA46968678890_082021-F.webp",
-        name: "Disco Engomado Olimpico",
-        price: 4.5,
-        rating: 2.5,
-        numReviews: 34,
-    },
-    {
-        isNew: true,
-        imageURL:
-            "https://http2.mlstatic.com/D_NQ_NP_2X_746024-MLA48945892843_012022-F.webp",
-        name: "Whey Protein",
-        price: 4.5,
-        rating: 4.5,
-        numReviews: 34,
-    },
-    {
-        isNew: true,
-        imageURL:
-            "https://http2.mlstatic.com/D_NQ_NP_2X_709580-MLA46968678890_082021-F.webp",
-        name: "Disco Engomado Olimpico",
-        price: 4.5,
-        rating: 2.5,
-        numReviews: 34,
-    },
+  {
+    isNew: true,
+    imageURL:
+      "https://http2.mlstatic.com/D_NQ_NP_2X_746024-MLA48945892843_012022-F.webp",
+    name: "Whey Protein",
+    price: 4.5,
+    rating: 4.5,
+    numReviews: 34,
+  },
+  {
+    isNew: true,
+    imageURL:
+      "https://http2.mlstatic.com/D_NQ_NP_2X_709580-MLA46968678890_082021-F.webp",
+    name: "Disco Engomado Olimpico",
+    price: 4.5,
+    rating: 2.5,
+    numReviews: 34,
+  },
+  {
+    isNew: true,
+    imageURL:
+      "https://http2.mlstatic.com/D_NQ_NP_2X_746024-MLA48945892843_012022-F.webp",
+    name: "Whey Protein",
+    price: 4.5,
+    rating: 4.5,
+    numReviews: 34,
+  },
+  {
+    isNew: true,
+    imageURL:
+      "https://http2.mlstatic.com/D_NQ_NP_2X_709580-MLA46968678890_082021-F.webp",
+    name: "Disco Engomado Olimpico",
+    price: 4.5,
+    rating: 2.5,
+    numReviews: 34,
+  },
 ];
 
 export default function ProductsPage() {
-    return (
-        <Flex alignItems="center" justifyContent="center" dir="row" wrap="wrap" >
-            {data.map((e) => {
-                return <Card product={e}></Card>;
-            })}
-        </Flex>
-    );
+  return (
+    <Flex alignItems="center" justifyContent="center" dir="row" wrap="wrap">
+      {data.map((e) => {
+        return <Card product={e}></Card>;
+      })}
+    </Flex>
+  );
 }
