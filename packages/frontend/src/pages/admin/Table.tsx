3883af217830b6a2354dace167914405abdb3ee0
unknown <tomyguagnini@gmail.com>
2023-04-04 20:01:44 -0300

    feat(front/Admin/Products): create table, add route admin navbar

---
+++ packages/frontend/src/pages/admin/Table.tsx
@@ -0,0 +1,139 @@
+import React, { useMemo } from "react";
+import { useTable } from "react-table";
+import { Column } from "react-table";
+import {
+    Table,
+    Thead,
+    Tbody,
+    Tr,
+    Th,
+    Td,
+    Button,
+    Flex,
+} from "@chakra-ui/react";
+
+interface Product {
+    id: number;
+    name: string;
+    description: string;
+    price: string;
+    stock: string;
+    brand: Brand;
+    category: Category;
+    brandId: number;
+    categoryId: number;
+}
+
+interface Brand {
+    id: number;
+    name: string;
+    description: string;
+}
+
+interface Category {
+    id: number;
+    name: string;
+    description: string;
+}
+
+const Tabla = ({ data }: { data: Product[] }) => {
+    const columns: Column<Product>[] = useMemo(
+        () => [
+            {
+                Header: "Name",
+                accessor: "name",
+            },
+            {
+                Header: "Description",
+                accessor: "description",
+            },
+
+            {
+                Header: "Price",
+                accessor: "price",
+            },
+            {
+                Header: "Stock",
+                accessor: "stock",
+            },
+            {
+                Header: "Brand",
+                accessor: "brand.name",
+            },
+            {
+                Header: "Category",
+                accessor: "category.name",
+            },
+            {
+                Header: "Actions",
+                accessor: "id",
+                Cell: ({ value }: { value: number }) => (
+                    <Flex>
+                        <Button
+                            onClick={() => handleEdit(value)}
+                            bg="yellow.300"
+                            mr="5px"
+                        >
+                            Edit
+                        </Button>
+                        <Button
+                            onClick={() => handleDelete(value)}
+                            bg="red.500"
+                            color="white"
+                        >
+                            Delete
+                        </Button>
+                    </Flex>
+                ),
+            },
+        ],
+        []
+    );
+
+    const handleEdit = (id: number) => {
+        // Implementar lógica de edición aquí
+    };
+
+    const handleDelete = (id: number) => {
+        // Implementar lógica de edición aquí
+    };
+
+    const tableData = useMemo(() => data, [data]);
+
+    const tableInstance = useTable({ columns, data: tableData });
+
+    const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+        tableInstance;
+
+    return (
+        <Table variant="simple" {...getTableProps()}>
+            <Thead>
+                {headerGroups.map((headerGroup) => (
+                    <Tr {...headerGroup.getHeaderGroupProps()}>
+                        {headerGroup.headers.map((column) => (
+                            <Th {...column.getHeaderProps()}>
+                                {column.render("Header")}
+                            </Th>
+                        ))}
+                    </Tr>
+                ))}
+            </Thead>
+            <Tbody {...getTableBodyProps()}>
+                {rows.map((row) => {
+                    prepareRow(row);
+                    return (
+                        <Tr {...row.getRowProps()}>
+                            {row.cells.map((cell) => (
+                                <Td {...cell.getCellProps()}>
+                                    {cell.render("Cell")}
+                                </Td>
+                            ))}
+                        </Tr>
+                    );
+                })}
+            </Tbody>
+        </Table>
+    );
+};
+
+export default Tabla;
--- packages/frontend/src/pages/admin/products.tsx
+++ packages/frontend/src/pages/admin/products.tsx
@@ -1,3 +1,52 @@
-export default function ProductsAdminPage() {
-  return <h1>Administración de Productos</h1>;
-}
+import React from "react";
+import Tabla from "./Table";
+import {Flex} from "@chakra-ui/react"
+import { useDispatch, useSelector } from "react-redux";
+import { RootState, AppDispatch } from "../../redux/store/index";
+import { useEffect, useState } from "react";
+import * as actions from "../../redux/actions/index";
+
+const ProductsAdminPage = () => {
+    const [orderBy, setOrder] = useState("name");
+    const [alphaOrder, setAlpha] = useState("asc");
+    const [currentPage, setPage] = useState("1");
+    const [brandFilter, setBrand] = useState(0);
+    const [categoryFilter, setCategory] = useState(0);
+
+    const dispatch: AppDispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(actions.getAllCategories());
+        dispatch(actions.getAllBrands());
+        dispatch(
+            actions.getAllProducts(
+                orderBy,
+                alphaOrder,
+                currentPage,
+                brandFilter,
+                categoryFilter
+            )
+        );
+    }, [
+        dispatch,
+        orderBy,
+        alphaOrder,
+        currentPage,
+        brandFilter,
+        categoryFilter,
+    ]);
+
+    const data = useSelector((state: RootState) => state.products);
+    const allItems = useSelector((state: RootState) => state.totalItems);
+    const cardsPerPage = useSelector((state: RootState) => state.cardsForPages);
+    const categories = useSelector((state: RootState) => state.categories);
+    const brands = useSelector((state: RootState) => state.brands);
+
+    return (
+        <Flex justifyContent="center">
+            <Tabla data={data} />;
+        </Flex>
+    );
+};
+
+export default ProductsAdminPage;
