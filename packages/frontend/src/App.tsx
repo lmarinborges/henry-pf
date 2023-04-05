a5b9a479c3da067c2f9ea6a9af60230605995f08
pablitomartinez <86635786+pablitomartinez@users.noreply.github.com>
2023-04-05 18:51:05 -0300

    fix(features/components/create-product-form): create products

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -16,6 +16,7 @@ import HomePage from "./pages/home";
 import NotFoundPage from "./pages/notfound";
 import ProductsPage from "./pages/products";
 import ProductPage from "./pages/products/product"
+import CreateProductForm from "./features/components/CreateProductForm";
 
 
 
@@ -29,7 +30,7 @@ const routes = createBrowserRouter(
         <Route path="contact" element={<ContactPage />} />
         <Route path="products" element={<ProductsPage />}/>
         <Route path="products/:productId" element={<ProductPage />} />
-        
+        <Route path="createProduct" element={<CreateProductForm />}/>
       </Route>
       <Route path="/admin" element={<AdminLayout />}>
         <Route index element={<Navigate to="products" />} />
