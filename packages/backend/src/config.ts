0d94c79feae8de3c5c7a24d0b0512200bf44a21e
Liam Marin <liam.marin03926@protonmail.com>
2023-04-06 19:54:12 -0300

    feat(backend): extract api to own module

--- packages/backend/src/config.ts
+++ packages/backend/src/config.ts
@@ -1,3 +1,4 @@
+import { SessionOptions } from "express-session";
 import parseDuration from "parse-duration";
 
 export const expressPort = () => {
@@ -6,14 +7,26 @@ export const expressPort = () => {
   return value;
 };
 
-export const sessionSecret = () => {
+const sessionSecret = () => {
   const value = process.env.SESSION_SECRET;
   if (!value) throw new Error("SESSION_SECRET is not set");
   return value;
 };
 
-export const sessionMaxAge = () => {
+const sessionMaxAge = () => {
   const value = parseDuration(process.env.SESSION_MAXAGE || "31d");
   if (value === null) throw new Error("SESSION_MAXAGE is invalid");
   return value;
+};
+
+export const sessionConfig: SessionOptions = {
+  secret: sessionSecret(),
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    maxAge: sessionMaxAge(),
+    httpOnly: true,
+    sameSite: "strict",
+    secure: process.env.NODE_ENV === "production",
+  },
 };
