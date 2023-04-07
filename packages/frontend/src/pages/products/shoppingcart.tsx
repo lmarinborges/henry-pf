3e19d7f9764712415d45bd984f2e60d1f9f453dd
Santiago N. Carrattini <sncarrattini@hotmail.com>
2023-04-06 22:09:28 -0300

    fix(products): shoppingcart_and_shoppingcard_and_product

--- packages/frontend/src/pages/products/shoppingcart.tsx
+++ packages/frontend/src/pages/products/shoppingcart.tsx
@@ -1,56 +1,68 @@
 import { Box, Heading, Text } from "@chakra-ui/react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import ShoppingCard from "./shoppingCard";
 
-export default function ShoppingCart(){
-    const [totalPrice,setTotalPrice]=useState(0);
+export default function ShoppingCart() {
+  const [totalPrice, setTotalPrice] = useState(0);
 
-    var storage:Array<any>=[];
+  var storage: Array<any> = [];
 
-
-    for(var i=0;i<localStorage.length;i++){
-        if(localStorage.key(i)?.includes("-")!==true)
-        {
-            storage.push(localStorage.key(i))
-        }
-    };
-    
-    const products=storage.map((e)=>{
-        let prod=localStorage.getItem(e);
-        if(prod!==null){
-            return JSON.parse(prod)
-        }
-        else return null
-    });
-
-    const totalValue=(val:number,rest:boolean)=>{
-        let result=0
-        if(rest===true){
-            result=(totalPrice-val)    
-            setTotalPrice(result)
-        }
-        else {
-            result=Number(totalPrice)+val;
-            setTotalPrice(result)
-        } 
-    };
-
-    const onClose= (name:string) => {
-        localStorage.removeItem(name);
-        window.location.reload()
+  for (var i = 0; i < localStorage.length; i++) {
+    if (localStorage.key(i)?.includes("chakra") !== true) {
+      storage.push(localStorage.key(i));
     }
+  }
 
-    var totalCards=products.map((e,i)=>{
-        return <ShoppingCard key={i} product={e} totalValue={totalValue} onClose={onClose}/>
-    });
-    
-    
+  const products = storage.map((e) => {
+    let prod = localStorage.getItem(e);
+    if (prod !== null) {
+      return JSON.parse(prod);
+    } else return null;
+  });
+
+  useEffect(() => {
+    if (totalPrice === 0) {
+      var init: number = 0;
+      products.forEach((e) => {
+        init += Number(e.price);
+      });
+      setTotalPrice(init);
+    }
+  }, [totalPrice, products]);
+
+  const totalValue = (val: number, rest: boolean) => {
+    let result = 0;
+    if (rest === true) {
+      result = totalPrice - val;
+      setTotalPrice(result);
+    } else {
+      result = Number(totalPrice) + val;
+      setTotalPrice(result);
+    }
+  };
+
+  const onClose = (name: string) => {
+    localStorage.removeItem(name);
+    window.location.reload();
+  };
+
+  var totalCards = products.map((e, i) => {
     return (
-        <Box bg="black" padding="10px" m="0%">
-            <Heading color="white">Carrito:</Heading>
-            {totalCards}
-            <Text color="white">Total: ${totalPrice.toFixed(2)}</Text>
-        </Box>
-    )
+      <ShoppingCard
+        key={i}
+        product={e}
+        totalValue={totalValue}
+        onClose={onClose}
+      />
+    );
+  });
+
+  return (
+    <Box bg="black" padding="10px" m="0%">
+      <Heading color="white">Carrito:</Heading>
+      {totalCards}
+      <Text color="white">Total: ${totalPrice.toFixed(2)}</Text>
+    </Box>
+  );
 }
