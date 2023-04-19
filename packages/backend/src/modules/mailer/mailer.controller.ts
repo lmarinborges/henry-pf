ccb889b96f47dad392721481fe4f2259763d4792
Nico <nico.tramontina@gmail.com>
2023-04-19 15:14:52 -0300

    feat: add subscription mail

--- packages/backend/src/modules/mailer/mailer.controller.ts
+++ packages/backend/src/modules/mailer/mailer.controller.ts
@@ -55,10 +55,13 @@ export async function sendSuscriptionMail(req: Request, res: Response) {
     from: process.env.EMAIL,
     to: req.body.to,
     subject: "Bienvenido a nuestra pagina",
-    html: `<div> <p> Gracias ${name} haberte registrado en nuestra pagina
-    web. Aqui
-    encontraras
-    lo mejores productos de gimnasio al mejor precio! </p></div>`,
+    html: `<div> <p>¡Gracias por suscribirte a nuestra página! Nos emociona tenerte como parte de nuestra comunidad y esperamos que disfrutes de los recursos y contenido que ofrecemos.</p>
+    <p>En nuestra página encontrarás información valiosa sobre [tema de la página], incluyendo artículos, tutoriales, guías, videos y mucho más. También ofrecemos la oportunidad de conectarte con otros miembros de la comunidad para compartir ideas, hacer preguntas y colaborar en proyectos.</p>
+    <p>Para mantenerte actualizado sobre nuestras últimas publicaciones y novedades, asegúrate de seguirnos en nuestras redes sociales [listar aquí las redes sociales de la página].</p>
+    <p>Si tienes alguna pregunta o comentario, no dudes en ponerte en contacto con nosotros. Estamos siempre dispuestos a ayudar y a escuchar tus sugerencias para mejorar nuestra página.</p>
+    <p>¡Gracias de nuevo por unirte a nuestra comunidad! Esperamos verte pronto en nuestra página.</p>
+    <p>Atentamente,</p>
+    <p>[Nombre del remitente]</p></div>`,
   });
   //console.log(result)
   return res.status(200).send("The mail was sent");
